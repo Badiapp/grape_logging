6b5617a4eb7c57e1ec04c4f5a49a61f052e38a8d
David Santos Merino <david@badiapp.com>
2020-09-23 10:50:53 +0200

    Fix format for Grape responses (#8)

--- lib/grape_logging/loggers/response.rb
+++ lib/grape_logging/loggers/response.rb
@@ -2,24 +2,37 @@ module GrapeLogging
   module Loggers
     class Response < GrapeLogging::Loggers::Base
       MAX_RESPONSE_LENGTH = 180_000
-      RESPONSE_LENGTH_EXCEEDED = "{\"alert\":\"response_length_exceeded\",\"alert_description\":\"Response length exceeded maximum allowed characters and was removed due to logging system constraints.\"}".freeze
+      MAX_RESPONSE_BODY = {
+        'alert': 'response_length_exceeded',
+        'alert_description':
+          'Response length exceeded maximum allowed characters and was removed due to logging system constraints.'
+      }.freeze
 
       def parameters(_, response)
         response ? { response: serialized_response_body(response) } : {}
       end
 
       private
+
       # In some cases, response.body is not parseable by JSON.
       # For example, if you POST on a PUT endpoint, response.body is egal to """".
       # It's strange but it's the Grape behavior...
       def serialized_response_body(response)
-        if response.body.body.length > MAX_RESPONSE_LENGTH
-          JSON.parse(RESPONSE_LENGTH_EXCEEDED)
-        else
-          JSON.parse(response.body.body)
-        end
+        body = if response.body.respond_to?(:body) # Rails responses
+                 response.body.body
+               else # Grape responses
+                 response.body.first
+               end
+
+        serialize_body(body)
       rescue StandardError
         response.body
+      end
+
+      def serialize_body(body)
+        return MAX_RESPONSE_BODY if body.length > MAX_RESPONSE_LENGTH
+
+        JSON.parse(body)
       end
     end
   end
